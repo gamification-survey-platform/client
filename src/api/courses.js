369f15b59089055a46eb569961f897d9f0d1ba6f
Vishaal Agartha <vishaalagartha@gmail.com>
2023-03-08 19:12:20 -0800

    Courses refactoring

--- src/api/courses.js
+++ src/api/courses.js
@@ -3,8 +3,6 @@ import api from './apiUtils'
 
 const createCourse = async (courseData) => {
   try {
-    if (!validateUser()) throw new Error('User is not authenticated')
-    if (!validateAdmin()) throw new Error('User does not have required role')
     const res = await api.post(`courses/`, courseData, {
       headers: { 'Content-Type': 'multipart/form-data' }
     })
@@ -16,8 +14,6 @@ const createCourse = async (courseData) => {
 
 const editCourse = async ({ course_id, course }) => {
   try {
-    if (!validateUser()) throw new Error('User is not authenticated')
-    if (!validateAdmin()) throw new Error('User does not have required role')
     const res = await api.put(`courses/`, course, {
       headers: { 'Content-Type': 'multipart/form-data' },
       params: { course_id }
@@ -30,8 +26,6 @@ const editCourse = async ({ course_id, course }) => {
 
 const deleteCourse = async (course_id) => {
   try {
-    if (!validateUser()) throw new Error('User is not authenticated')
-    if (!validateAdmin()) throw new Error('User does not have required role')
     const res = await api.delete('courses/', { params: { course_id } })
     return res
   } catch (error) {
@@ -41,7 +35,6 @@ const deleteCourse = async (course_id) => {
 
 const getUserCourses = async (andrewId) => {
   try {
-    if (!validateUser()) throw new Error('User is not authenticated')
     const res = await api.get('courses/', { params: { andrewId } })
     return res
   } catch (error) {
@@ -51,7 +44,6 @@ const getUserCourses = async (andrewId) => {
 
 const getCourse = async (course_id) => {
   try {
-    if (!validateUser()) throw new Error('User is not authenticated')
     const res = await api.get('courses/', { params: { course_id } })
     return res
   } catch (error) {
--- src/pages/courses/Courses.jsx
+++ src/pages/courses/Courses.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
-import { Alert, Container, Table, Button } from 'react-bootstrap'
+//import { Alert, Container, Table, Button } from 'react-bootstrap'
+import { Table, Button, Alert, Tag } from 'antd'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { deleteCourse as deleteCourseApi } from '../../api/courses'
 import { deleteCourse } from '../../store/courses/coursesSlice'
 import coursesSelector from '../../store/courses/selectors'
 import userSelector from '../../store/user/selectors'
+import { LinkContainer } from 'react-router-bootstrap'
+import { isInstructorOrTA } from '../../utils/roles'
 
 const Courses = () => {
   const navigate = useNavigate()
@@ -13,8 +16,9 @@ const Courses = () => {
   const courses = useSelector(coursesSelector)
   const [showError, setShowError] = useState(false)
   const dispatch = useDispatch()
+  const dataSource = courses.map((c, i) => ({ ...c, key: i }))
 
-  const handleDeleteCourse = async (e, coursePk) => {
+  const handleDeleteCourse = async (coursePk) => {
     try {
       const res = await deleteCourseApi(coursePk)
       if (res.status === 200) {
@@ -26,74 +30,90 @@ const Courses = () => {
     }
   }
 
+  const columns = [
+    { title: 'Course Name', dataIndex: 'course_name', align: 'center' },
+    { title: 'Course Number', dataIndex: 'course_number', key: 'course_number', align: 'center' },
+    { title: 'Semester', dataIndex: 'semester', key: 'semester', align: 'center' },
+    {
+      title: '',
+      dataIndex: 'assignments',
+      key: 'assignments',
+      render: (_, course) => {
+        return (
+          <LinkContainer
+            to={`/courses/${course.course_number}/assignments`}
+            style={{ textAlign: 'center' }}>
+            <Tag role="button" color="blue">
+              Assignments
+            </Tag>
+          </LinkContainer>
+        )
+      }
+    },
+    {
+      title: '',
+      dataIndex: 'members',
+      key: 'members',
+      render: (_, course) => {
+        return (
+          <LinkContainer to={`/courses/${course.course_number}/members`}>
+            <Tag role="button" color="purple">
+              Members
+            </Tag>
+          </LinkContainer>
+        )
+      }
+    },
+    {
+      title: '',
+      dataIndex: 'view',
+      key: 'view',
+      render: (_, course) => {
+        return (
+          <LinkContainer to={`/courses/${course.course_number}/details`}>
+            <Tag role="button" color="cyan">
+              View
+            </Tag>
+          </LinkContainer>
+        )
+      }
+    },
+    {
+      title: '',
+      dataIndex: 'edit',
+      key: 'edit',
+      render: (_, course) => {
+        return isInstructorOrTA(course.user_role) ? (
+          <LinkContainer to={`/courses/${course.course_number}/edit`}>
+            <Tag role="button" color="gold">
+              Edit
+            </Tag>
+          </LinkContainer>
+        ) : null
+      }
+    },
+    {
+      title: '',
+      dataIndex: 'delete',
+      key: 'delete',
+      render: (_, course) =>
+        isInstructorOrTA(course.user_role) ? (
+          <Tag role="button" color="red" onClick={() => handleDeleteCourse(course.pk)}>
+            Delete
+          </Tag>
+        ) : null
+    }
+  ]
+
   return (
-    <Container className="mt-5 text-center">
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Course Name</th>
-            <th>Course Number</th>
-            <th>Semester</th>
-          </tr>
-        </thead>
-        <tbody>
-          {courses.map((course, i) => {
-            return (
-              <tr key={i}>
-                <td>{course.course_name}</td>
-                <td>{course.course_number}</td>
-                <td>{course.semester}</td>
-                <td>
-                  <Link to={`/courses/${course.course_number}/assignments`}>
-                    <Button className="mx-3">Assignments</Button>
-                  </Link>
-                </td>
-                <td>
-                  <Link to={`/courses/${course.course_number}/members`}>
-                    <Button className="mx-3" variant="secondary">
-                      Members
-                    </Button>
-                  </Link>
-                </td>
-                <td>
-                  <Link to={`/courses/${course.course_number}/details`}>
-                    <Button className="mx-3" variant="info">
-                      View
-                    </Button>
-                  </Link>
-                </td>
-                {course.user_role !== 'Student' && (
-                  <>
-                    <td>
-                      <Link to={`/courses/${course.course_number}/edit`}>
-                        <Button className="mx-3" variant="warning">
-                          Edit
-                        </Button>
-                      </Link>
-                    </td>
-                    <td>
-                      <Button
-                        className="mx-3"
-                        variant="danger"
-                        onClick={(e) => handleDeleteCourse(e, course.pk)}>
-                        Delete
-                      </Button>
-                    </td>
-                  </>
-                )}
-              </tr>
-            )
-          })}
-        </tbody>
-      </Table>
+    <div className="m-5">
+      <Table className="text-center" columns={columns} dataSource={dataSource} />
       {user && user.is_staff && (
         <Button className="my-5" onClick={() => navigate('/courses/add')}>
           Add Course
         </Button>
       )}
-      {showError && <Alert variant="danger">Failed to delete course.</Alert>}
-    </Container>
+      {showError && <Alert type="error" message="Failed to delete course" showIcon />}
+    </div>
   )
 }
-
-export default Courses
