461d6dd2b586d54ba67c1a7818c2c0ac8c14d537
rhuang4F2023 <rhuang4@andrew.cmu.edu>
2023-09-24 22:39:48 -0700

    disableBeacon

--- src/components/DashboardJoyride.jsx
+++ src/components/DashboardJoyride.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
 import Joyride, { STATUS } from 'react-joyride';
 import { QuestionCircleOutlined } from '@ant-design/icons';
+import React, { useState, useEffect } from 'react';
 
 const DashboardJoyride = () => {
   const [run, setRun] = useState(false);
@@ -11,54 +11,70 @@ const DashboardJoyride = () => {
     setJoyrideKey(joyrideKey + 1);
   };
 
+  useEffect(() => {
+    console.log("Run:", run);
+  }, [run]);  
+
   const steps = [
     {
       target: '.Dashboard',
       content: 'This is your dashboard. You can find all the information about your progress here.',
-      placement: 'right'
+      placement: 'right',
+      disableBeacon: true
     },
     {
       target: '.profile',
       content: 'This is your profile. You can edit and upload your picture here.',
-      placement: 'right'
+      placement: 'right',
+      disableBeacon: true
     },
     {
       target: '.courses',
       content: 'Here you can find your courses.',
-      placement: 'right'
+      placement: 'right',
+      disableBeacon: true
     },
     {
       target: '.store',
       content: 'This is the store. You can find various items here.',
-      placement: 'right'
+      placement: 'right',
+      disableBeacon: true
     },
     {
       target: '.theme',
       content: 'You can customize the theme of your dashboard from here.',
-      placement: 'right'
+      placement: 'right',
+      disableBeacon: true
     },
     {
         target: '.leaderboard',
         content: 'This is the leaderboard. You can view your rankings here.',
+        placement: 'right',
+        disableBeacon: true
     },
     {
         target: '.guide',
         content: 'This is the guide. You can find helpful information about using the platform here.',
+        placement: 'right',
+        disableBeacon: true
     },
     {
         target: '.daily-streak',
         content: 'This shows your daily streak. Keep logging in daily to increase it!',
         placement: 'bottom',
+        disableBeacon: true
     },
     {
         target: '.notification',
         content: 'Here you can see your notifications. Click to view them.',
         placement: 'bottom',
+        disableBeacon: true
     },
     {
         target: '.logout',
         content: 'Click here to log out of your account.',
         placement: 'bottom',
+        disableBeacon: true
     },
   ];
 
