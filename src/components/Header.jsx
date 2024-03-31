5f931eeb130324b0e2167c217919d203e44f84eb
rhuang4F2023 <rhuang4@andrew.cmu.edu>
2024-03-31 12:58:21 -0700

    move out

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,10 +34,7 @@ import Notification from './Notification'
 import styles from '../styles/Header.module.css'
 import DashboardJoyride from './DashboardJoyride'
 import { gamified_mode } from '../gamified'
-import AvatarBadge from '../assets/badges/Avatar-Alchemist.png'
-import CommentCaptainSilverBadge from '../assets/badges/Silver/Comment-Captain.png' 
-import CommentCaptainGoldBadge from '../assets/badges/Gold/Comment-Captain.png'
-import PeerReviewGoldBadge from '../assets/badges/Gold/Peer-review-provided.png'
+import BadgeContainer from './BadgeContainer';
 
 const rankings = [
   { title: 'Bronze', image: Bronze },
@@ -183,42 +180,6 @@ const AppHeader = ({ children }) => {
     }
   })
 
-  // const BadgeContainer = () => (
-  //   <div style={{ 
-  //     padding: '0px 0px',
-  //     background: '#FCC200', 
-  //     borderRadius: '15px', 
-  //     transition: 'transform 0.3s ease',
-  //     display: 'inline-flex',
-  //     alignItems: 'center',
-  //     justifyContent: 'center',
-  //   }}
-  //   onMouseOver={(e) => e.currentTarget.style.transform = 'scale(1.05)'}
-  //   onMouseOut={(e) => e.currentTarget.style.transform = 'scale(1)'}
-  //   >
-  //     <Tooltip title="Avatar Alchemist" color={'#FFDF00'} placement="left">
-  //       <div style={{ margin: '0 4px' }}>
-  //         <Image src={AvatarBadge} preview={false} width={50} height={50} />
-  //       </div>
-  //     </Tooltip>
-  //     <Tooltip title="Comment Captain" color={'#E6E8FA'} placement="left">
-  //       <div style={{ margin: '0 4px' }}>
-  //         <Image src={CommentCaptainSilverBadge} preview={false} width={50} height={50} />
-  //       </div>
-  //     </Tooltip>
-  //     <Tooltip title="Comment Captain" color={'#FFDF00'} placement="left">
-  //       <div style={{ margin: '0 4px' }}>
-  //         <Image src={CommentCaptainGoldBadge} preview={false} width={50} height={50} />
-  //       </div>
-  //     </Tooltip>
-  //     <Tooltip title="Peer Reviewer" color={'#CD7F32'} placement="left">
-  //       <div style={{ margin: '0 4px' }}>
-  //         <Image src={PeerReviewGoldBadge} preview={false} width={50} height={50} />
-  //       </div>
-  //     </Tooltip>
-  //   </div>
-  // );
-
   return (
     <Layout style={{ minHeight: '100vh' }}>
       {contextHolder}
@@ -251,7 +212,7 @@ const AppHeader = ({ children }) => {
             <Image src={Logo} preview={false} width={300} />
           </LinkContainer>
           <div>
-            {/* <BadgeContainer /> */}
+            <BadgeContainer />
           </div>
           <Row>
             <Tooltip title="Start Tutorial" color={'gold'} placement="left">
