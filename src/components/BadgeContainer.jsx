fe879963c2097dd8ecd89280982cae7655cc079a
rhuang4F2023 <rhuang4@andrew.cmu.edu>
2024-03-31 14:46:43 -0700

    framer-motion

--- src/components/BadgeContainer.jsx
+++ src/components/BadgeContainer.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import { Image, Tooltip } from 'antd';
+import { motion } from 'framer-motion';
 import AvatarBadge from '../assets/badges/Avatar-Alchemist.png';
 
 const BadgeContainer = ({ user }) => {
+  const variants = {
+    hidden: { scale: 0, opacity: 0 },
+    visible: { scale: 1, opacity: 1, transition: { duration: 0.5 } },
+  };
+
   return (
     <div
       style={{
         padding: '0px',
         background: '#FCC200',
         borderRadius: '15px',
-        transition: 'transform 0.3s ease',
         display: 'inline-flex',
         alignItems: 'center',
         justifyContent: 'center',
       }}
-      onMouseOver={(e) => (e.currentTarget.style.transform = 'scale(1.05)')}
-      onMouseOut={(e) => (e.currentTarget.style.transform = 'scale(1)')}
     >
       {user && user.image && (
         <Tooltip title="Avatar Alchemist" color={'#FFDF00'} placement="left">
           <div style={{ margin: '0 4px' }}>
-            <Image src={AvatarBadge} preview={false} width={50} height={50} />
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              variants={variants}
+            >
+              <Image src={AvatarBadge} preview={false} width={50} height={50} />
+            </motion.div>
           </div>
         </Tooltip>
       )}
