82cc0f621d73bcdc989f31303c3155cb7e734f82
Hanqi Dai <hanqid@andrew.cmu.edu>
2024-04-02 20:08:55 -0700

    eslint

--- src/api/historicalReview.js
+++ src/api/historicalReview.js
@@ -1,24 +1,23 @@
 import api from '../api/apiUtils'
 
 const getHistoricalArtifactReviews = async () => {
-    try {
-      const res = await api.get(`historical_reviews/`)
-      console.log(res)
-      return res
-    } catch (error) {
-      throw new Error(error.response.data.message)
-    }
+  try {
+    const res = await api.get(`historical_reviews/`)
+    console.log(res)
+    return res
+  } catch (error) {
+    throw new Error(error.response.data.message)
+  }
 }
 
 const getAnswerHistory = async () => {
-    try {
-      const res = await api.get(`answer_history/`)
-      console.log(res)
-      return res
-    } catch (error) {
-      throw new Error(error.response.data.message)
-    }
+  try {
+    const res = await api.get(`answer_history/`)
+    console.log(res)
+    return res
+  } catch (error) {
+    throw new Error(error.response.data.message)
+  }
 }
-
 
 export { getHistoricalArtifactReviews, getAnswerHistory }
